2ee1e116f886e7b4a59c50b505f129192f3b3dfb
msuslow <78159724+msuslow@users.noreply.github.com>
2022-03-14 09:44:41 -0400

    Create config.js
    updated config file

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_8MGtsPTrL', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3ohhk9lin02hfp6jekqudsmjeg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1'
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
